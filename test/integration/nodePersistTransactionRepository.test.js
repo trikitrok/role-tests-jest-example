52155c2addc61defb1591fc5ac0c3ea8b9bd4a32
trikitrok <trikitrok@gmail.com>
2022-08-03 12:01:56 +0200

    using role tests

---
+++ test/integration/nodePersistTransactionRepository.test.js
@@ -0,0 +1,33 @@
+import crypto from "crypto";
+import fs from 'fs';
+import {NodePersistTransactionRepository} from "../../src/nodePersistTransactionRepository";
+import {behavesLikeATransactionRepository} from "./transactionRepository.role";
+
+const filePath = `./tmp/${md5('transactions')}`;
+
+describe('node-persist transaction repository', behavesLikeATransactionRepository({
+    init() {
+        fs.rmSync('./tmp', {force: true, recursive: true});
+        fs.mkdirSync('./tmp');
+    },
+    clean() {
+        fs.rmSync('./tmp', {force: true, recursive: true});
+    },
+    getInstance() {
+        return new NodePersistTransactionRepository('./tmp');
+    },
+    readTransactions() {
+        let content = fs.readFileSync(filePath, {encoding: 'utf-8'});
+        return JSON.parse(content).value.map(t => ({...t, date: new Date(t.date)}));
+    },
+    writeTransactions(transactions) {
+        fs.writeFileSync(filePath, JSON.stringify({
+            key: 'transactions',
+            value: transactions
+        }), {encoding: 'utf-8'});
+    }
+}));
+
+function md5(key) {
+    return crypto.createHash('md5').update(key).digest('hex');
+}
